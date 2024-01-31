0d5f88c55ee15166be134e341b1716ad5406e082
ATN20222 <antonabdalla30@gmail.com>
2024-01-31 16:54:20 +0200

    edit colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ window.onscroll = function() {
             nav.style.transition = ' all .5s ease-in-out';
             nav.style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px';
             if(scrollPosition > firstsec){
-                nav.style.backgroundColor = 'rgba(0, 0, 0, 0.774)';
+                nav.style.backgroundColor = 'white';
             }
 
         }else{
