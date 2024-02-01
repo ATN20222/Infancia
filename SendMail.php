f3a5dc82ad819a634c6c4703f979a7f550467058
ATN20222 <antonabdalla30@gmail.com>
2024-02-01 14:58:13 +0200

    delete files

--- SendMail.php
+++ SendMail.php
@@ -1,33 +1,20 @@
-
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
+    $to = "aaballa@mindholding.net";
+    $headers = "From: $name <$email>\r\n";  
+    $email_message = "Subject: $subject\n\nMessage:\n$message";
+    $result =mail($to, $subject, $email_message, $headers);
 
-if(isset($_POST['send'])){
-    $name = htmlentities($_POST['name']);
-    $email = htmlentities($_POST['email']);
-    $subject = htmlentities($_POST['subject']);
-    $message = htmlentities($_POST['message']);
-
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'aabdalla@mindholding.net';
-    $mail->Password = 'Mind12345@';
-    $mail->Port = 587;
-    $mail->SMTPSecure = 'ssl';
-    $mail->isHTML(true);
-    $mail->setFrom($email, $name);
-    $mail->addAddress('aabdalla@mindholding.net');
-    $mail->Subject = ("$email ($subject)");
-    $mail->Body = $message;
-    $mail->send();
-
-    header("Location: ./index.html");
+    if ($result) {  
+        header("Location: index.html");
+        exit();
+    } else {
+        echo "Failed to send email.";
+    }
 }
 ?>
