8b2c18d07afce1439a9db5ffd502b893fa1cc4e6
ATN20222 <antonabdalla30@gmail.com>
2024-02-05 14:21:09 +0200

    add sending mail

--- SendMail.php
+++ SendMail.php
@@ -1,20 +1,48 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
 
-    $to = "aaballa@mindholding.net";
-    $headers = "From: $name <$email>\r\n";  
-    $email_message = "Subject: $subject\n\nMessage:\n$message";
-    $result =mail($to, $subject, $email_message, $headers);
+// Declare variables
+$name = $_POST["name"];
+$email = $_POST["email"];
+$NurserySize = $_POST["NurserySize"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-    if ($result) {  
-        header("Location: index.html");
-        exit();
-    } else {
-        echo "Failed to send email.";
-    }
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp-mail.outlook.com';                    
+    $mail->SMTPAuth   = true;                            
+    $mail->Username   = 'info@infancia.app';                    
+    $mail->Password   = 'Mind12345@';                              
+    $mail->SMTPSecure = 'tls';            
+    $mail->Port       = 587;                                    
+
+    //Recipients
+    $mail->setFrom('info@infancia.app', $name);
+    $mail->addAddress('info@infancia.app');     
+
+    $mail->isHTML(true);                                  
+    $mail->Subject = $subject;
+    $mail->Body    = "<p><strong>From:</strong> $name &lt;$email&gt;</p><br><p><strong>Nursery Size:</strong> $NurserySize</p><br>" . $message ."<br><p>this mail from infancia.app</p>";
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    // echo 'Message has been sent';
+    header('Location: ' . 'sent.html');
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
